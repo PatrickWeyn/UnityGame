d36b4fd6e75ee5a3bb147c5acbdeeafd5109e1a3
PatrickWeyn <patrick-weyn@hotmail.be>
2021-08-04 15:34:15 +0200

    Rigidbody
    Added Rigidbody on Player Creatures.
    Removed gravity on Rigidbody on Y-axis.
    Removed rotation on Rigidbody.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : Creature
 {
-
+    protected override void Start() {
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+        gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+    }
 }
