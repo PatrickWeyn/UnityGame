93564fb7b3192f3376a7565ed3dca86b21706ae0
PatrickWeyn <patrick-weyn@hotmail.be>
2021-08-04 12:59:42 +0200

    Initialize + Capture Input + Sprite Movement Direction
    Detects input on zasd
    Changes sprite direction based on mouse position relative to center of the screen

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Creature : MonoBehaviour
+{
+
+    public float speedx;
+    public float speedy;
+
+    protected virtual void FixedUpdate() {
+
+        //Capture Input
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //Determine the difference between our current position and the new position
+        Vector3 movedelta = new Vector3(x * speedx, y * speedy, 0);
+
+        //Swap sprite direction based on mouse position
+        if ((Input.mousePosition.x- Screen.width / 2) <0) {
+            transform.localScale = new Vector3(-1, 1, 0);
+        }
+        else {
+            transform.localScale = new Vector3(1, 1, 0);
+        }
+
+    }
+
+}
