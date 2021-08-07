181a79883dc47f36434c6766d8088227e1499ea6
PatrickWeyn <patrick-weyn@hotmail.be>
2021-08-07 12:38:38 +0200

    Fixed NPC sprite direction bug There was a bug where the NPCs looked in the same direction as the player at all times. This has been fixed.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,4 +9,14 @@ public class Player : Creature
         gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
         gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
     }
+
+    protected override void ChangeSpriteDirection(Vector3 dir) {
+        //Swap sprite direction based on mouse position
+        if ((Input.mousePosition.x - Screen.width / 2) < 0) {
+            transform.localScale = new Vector3(-1, 1, 0);
+        }
+        else {
+            transform.localScale = new Vector3(1, 1, 0);
+        }
+    }
 }
