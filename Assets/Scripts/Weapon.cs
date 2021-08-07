4548ebbf592fcde8ef8ff458de8e5d944d8a6539
PatrickWeyn <patrick-weyn@hotmail.be>
2021-08-07 21:19:04 +0200

    Function Swing Added a function that allows a weapon to be swung. Additionally, cleaned the animation. Looks good now.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,11 +7,33 @@ public class Weapon : MonoBehaviour
     public int mindam;
     public int maxdam;
     public float pushback;
+
+    private Animator anim;
     public float cooldown;
     private float lastswing;
+
+    private void Start() {
+        anim = GetComponent<Animator>();
+    }
 
     public void OnCollisionStay2D(Collision2D collision) {
         if (collision.collider.tag == "Fighter")
             collision.gameObject.SendMessage("ReceiveDamage", new Damage(Random.Range(mindam, maxdam), pushback, this.transform.parent.gameObject));
+    }
+
+    private void Swing() {
+        if (Time.time - lastswing > cooldown) { 
+        lastswing = Time.time;
+        anim.SetTrigger("Swing");
+        }
+        else {
+            Debug.Log("Your swing is on cooldown!");
+        }
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0) && gameObject.transform.parent.name == "Player") {
+            Swing();
+        }
     }
 }
