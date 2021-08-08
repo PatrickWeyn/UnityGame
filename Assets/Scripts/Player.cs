aff9e35852dcf56fb40729b7d56d7e0b0c8b4f5d
PatrickWeyn <patrick-weyn@hotmail.be>
2021-08-08 14:50:00 +0200

    UI: Ability Score Panel Added ability score panel to UI. Player lost immunity frames, but gained ability scores (initial value = 1).

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : Creature {
 
+    //Player Statistics
+    private int STR;
+    private int DEX;
+    private int CON;
+    private int INT;
+    private int WIS;
+    private int CHA;
+
     public int experience;
-    private float immuneperiod= 2.0f;
-    private float lasthitreceived;
 
     private void Start() {
         gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
         gameObject.GetComponent<Rigidbody2D>().freezeRotation = true;
+        STR = 1;
+        DEX = 1;
+        CON = 1;
+        INT = 1;
+        WIS = 1;
+        CHA = 1;
     }
 
     protected override void ChangeSpriteDirection(Vector3 dir) {
@@ -30,12 +42,5 @@ public class Player : Creature {
         float y = Input.GetAxisRaw("Vertical");
 
         this.MoveCreature(new Vector3(x, y, 0));
-    }
-
-    protected override void ReceiveDamage(Damage dmg) {
-        if (Time.time - lasthitreceived > immuneperiod) {
-            lasthitreceived = Time.time;
-            base.ReceiveDamage(dmg);
-        }
     }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -22,9 +22,13 @@ public class UIController : MonoBehaviour
         GameObject weaponpanel = transform.Find("Container").Find("CharacterWeapon").gameObject;
         Weapon weapon = GameManager.app.player.transform.Find("Weapon").GetComponent<Weapon>();
         weaponpanel.transform.Find("MinDmg").GetComponent<Text>().text = weapon.weapon.mindmg.ToString();
-        weaponpanel.transform.Find("MaxDmg").GetComponent<Text>().text = weapon.weapon.maxdmg.ToString();
+        weaponpanel.transform.Find("MaxDmg").GetComponent<Text>().text = (weapon.weapon.maxdmg-1).ToString();
         weaponpanel.transform.Find("Pushback").GetComponent<Text>().text = weapon.weapon.pushback.ToString();
         weaponpanel.transform.Find("Speed").GetComponent<Text>().text = weapon.weapon.cooldown.ToString();
         weaponpanel.transform.Find("WeaponImage").GetComponent<Image>().sprite = weapon.weapon.art;
+    }
+
+    public void UpdateStats() {
+
     }
 }
